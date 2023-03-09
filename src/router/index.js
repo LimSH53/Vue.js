b2e3a9ff532d529b9f341e34c973818d714d18b1
LimSH <tgtg5174@gmail.com>
2023-03-09 10:11:32 +0900

    https://github.com/LimSH53/Vue.js.git

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding/string',
+    name: 'DatabindingStringView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingStringView.vue'
+      )
+  },
+  {
+    path: '/databinding/html',
+    name: 'DatabindingHtmlView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingHtmlView.vue'
+      )
+  },
+  {
+    path: '/databinding/input',
+    name: 'DatabindingInputView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingInputView.vue'
+      )
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p></p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'suhyeon',
+      message: 'hihi'
+    }
+  }
+}
+</script>
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
